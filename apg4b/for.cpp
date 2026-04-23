8623b9a471f9cdb80a08529175f959cfb64ad9da
f42 <f42oct@gmail.com>
2026-04-23 20:14:10 +0900

    cleanse

--- apg4b/for.cpp
+++ apg4b/for.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     for (int i=0; i >5; i++){
-        cout <<"fuck you" <<endl;
+        cout <<"love you" <<endl;
     }
 }
 // byte, ms, kiB
--- apg4b/while.cpp
+++ apg4b/while.cpp
@@ -3,8 +3,7 @@ using namespace std;
 int main(){
     int i =0; //need to initialize here
     while (i <5){
-        cout <<"fuck you" <<endl;
+        cout <<"love you" <<endl;
         i++;
     }
 }
-// byte, AC, ms, kiB
